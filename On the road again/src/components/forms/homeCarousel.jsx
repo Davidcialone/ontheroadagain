4b61f7ef7effebfd1d42279d436161f9e3791406
Davidcialone <cialonedavid@gmail.com>
2024-09-12 16:02:17 +0200

    github pages deployement

--- On the road again/src/components/forms/homeCarousel.jsx
+++ On the road again/src/components/forms/homeCarousel.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import des styles du carousel
 
@@ -6,18 +5,17 @@ export const HomeCarousel = () => {
   return (
     <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true} interval={5000}>
       <div>
-        <img src=".\lisbonne.jpg"  alt="lisbonne" style={{ width: '100%' }}/>
-        </div>
-      <div>
-        <img src='.\mexique.jpg'alt="mexique" style={{ width: '100%' }}/>
+        <img src="/ontheroadagain/public/lisbonne.jpg" alt="lisbonne" style={{ width: '100%' }}/>
       </div>
       <div>
-        <img src='.\porto.jpg' alt="porto" style={{ width: '100%' }}/>
+        <img src="/ontheroadagain/public/mexique.jpg" alt="mexique" style={{ width: '100%' }}/>
       </div>
       <div>
-        <img src='.\verone.jpg' alt="verone" style={{ width: '100%' }}/>
+        <img src="/ontheroadagain/public/porto.jpg" alt="porto" style={{ width: '100%' }}/>
+      </div>
+      <div>
+        <img src="/ontheroadagain/public/verone.jpg" alt="verone" style={{ width: '100%' }}/>
       </div>
     </Carousel>
   );
 }
-
