bf1bfe8a987cb843f1a0283d509456daaa799cdb
Davidcialone <cialonedavid@gmail.com>
2024-09-13 17:57:38 +0200

    new try for photos

--- On the road again/src/components/forms/homeCarousel.jsx
+++ On the road again/src/components/forms/homeCarousel.jsx
@@ -5,16 +5,16 @@ export const HomeCarousel = () => {
   return (
     <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true} interval={5000}>
       <div>
-        <img src="On the road again\public\lisbonne.jpg" alt="lisbonne" style={{ width: '100%' }}/>
+        <img src="On the road again/public/lisbonne.jpg" alt="lisbonne" style={{ width: '100%' }}/>
       </div>
       <div>
-        <img src="On the road again\public\mexique.jpg" alt="mexique" style={{ width: '100%' }}/>
+        <img src="On the road again/public/mexique.jpg" alt="mexique" style={{ width: '100%' }}/>
       </div>
       <div>
-        <img src="On the road again\public\porto.jpg" alt="porto" style={{ width: '100%' }}/>
+        <img src="On the road again/public/porto.jpg" alt="porto" style={{ width: '100%' }}/>
       </div>
       <div>
-        <img src="On the road again\public\verone.jpg" alt="verone" style={{ width: '100%' }}/>
+        <img src="On the road again/public/verone.jpg" alt="verone" style={{ width: '100%' }}/>
       </div>
     </Carousel>
   );
