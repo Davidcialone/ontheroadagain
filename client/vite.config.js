287acba063e7e07c533d4d14f363e94f8a796011
Davidcialone <cialonedavid@gmail.com>
2024-10-28 15:32:58 +0100

    fix deploy 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,18 @@ import path from "path";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Ajoutez des plugins supplémentaires si nécessaire
+  ],
   base: "/ontheroadagain/",
   root: path.resolve(__dirname, ""),
   build: {
-    outDir: "dist", // Ensure the output directory is set to 'dist'
+    outDir: "dist", // Assurez-vous que le répertoire de sortie est défini sur 'dist'
+    rollupOptions: {
+      // Excluez jwt-decode si vous le souhaitez
+      external: ["jwt-decode"],
+    },
   },
   server: {
     port: 3000,
@@ -16,13 +23,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      process: "process/browser", // Polyfill for 'process'
-      buffer: "buffer", // Polyfill for 'Buffer' if needed
+      process: "process/browser", // Polyfill pour 'process'
+      buffer: "buffer", // Polyfill pour 'Buffer' si nécessaire
     },
   },
   optimizeDeps: {
     esbuildOptions: {
-      // Polyfill for Node.js globals like process and Buffer
+      // Polyfill pour les globales de Node.js comme process et Buffer
       plugins: [
         NodeGlobalsPolyfillPlugin({
           buffer: true,
