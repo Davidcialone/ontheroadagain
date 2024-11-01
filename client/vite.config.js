fca247688e2262f43f79bf271742cd74a5cc8061
Davidcialone <cialonedavid@gmail.com>
2024-11-01 11:32:54 +0100

    deploy 13

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,20 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/leaflet/dist/leaflet.css",
+          dest: "assets",
+        },
+      ],
+    }),
+  ],
   root: path.resolve(__dirname, ""),
   build: {
     outDir: "dist",
