e829535a30e75fba3a5728fec3fba6726c830472
Davidcialone <cialonedavid@gmail.com>
2024-11-01 14:54:46 +0100

    deploy 52

--- client/vite.config.js
+++ client/vite.config.js
@@ -39,7 +39,8 @@ export default defineConfig({
         "@mui/icons-material",
         "@mui/icons-material/CalendarToday",
         "leaflet/dist/leaflet.css",
-        "@mui/icons-material/CalendarToday",
+        "@mui/icons-material/Edit",
+        "@mui/icons-material/Delete",
       ],
       output: {
         assetFileNames: (assetInfo) => {
