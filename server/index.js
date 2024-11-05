a9320db938e6c105bcce46fada5a142977fec0e9
Davidcialone <cialonedavid@gmail.com>
2024-11-05 10:47:23 +0100

    adjust vercel

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import apiRouter from "./server.js"; // Import your API router
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
