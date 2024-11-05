f1d832122aa9e333462537bd22fda57f0ed186d9
Davidcialone <cialonedavid@gmail.com>
2024-11-05 12:49:23 +0100

    fix errors before for deploying

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import '../src/style/index.css';
+import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
