9280278f6b22b235cfeaa742d3e64596d762f54f
Davidcialone <cialonedavid@gmail.com>
2024-11-11 10:52:49 +0100

    fix supabse et some api routes

--- server/index.js
+++ server/index.js
@@ -79,9 +79,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(process.cwd(), "client/dist", "index.html"));
 });
 
-// Route de vérification de l'état du serveur
 app.get("/", (req, res) => {
-  res.send("API est en cours d'exécution");
+  res.redirect("/api");
 });
 
 // Initialisation de la base de données (non exécutable en production)
